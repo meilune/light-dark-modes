3a72eff50e41334093a8bd4b5a70322f3cc6a249
Ginja <virginija.meilune@gmail.com>
2022-08-09 09:22:56 +1000

    Finishing up the project

--- app.js
+++ app.js
@@ -1,19 +1,28 @@
 const sunIcon = document.getElementById("sun");
 const moonIcon = document.getElementById("moon");
-const switcher = document.querySelector('input[name=theme]');
-
-function toggleSwitch() {
-    if (switcher.style.display === "none") {
-        switcher.style.display = "block";
-    } else {
-
-    }
-}
+const switcher = document.getElementById("switch");
 
 switcher.addEventListener('change', () => {
-    if(this.checked) {
+    if(switcher.checked) {
+        moonIcon.hidden = false;
+        sunIcon.hidden = true;
+        localStorage.setItem('theme', 'dark');
         document.documentElement.setAttribute('data-theme', 'dark');
     } else {
+        moonIcon.hidden = true;
+        sunIcon.hidden = false;
+        localStorage.setItem('theme', 'light');
         document.documentElement.setAttribute('data-theme', 'light');
     }
 });
+
+//Check local storage
+const currentTheme = localStorage.getItem('theme');
+if(currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+    if(currentTheme === "dark") {
+        switcher.checked = true;
+        moonIcon.hidden = false;
+        sunIcon.hidden = true;
+    }
+}
