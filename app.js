897f7b3f8847e03e441aab8b05093043571f3311
Ginja <virginija.meilune@gmail.com>
2022-08-09 08:39:24 +1000

    Fixing the responsive mode

--- app.js
+++ app.js
@@ -1,4 +1,19 @@
 const sunIcon = document.getElementById("sun");
 const moonIcon = document.getElementById("moon");
-const switcher = document.getElementById("switch");
+const switcher = document.querySelector('input[name=theme]');
 
+function toggleSwitch() {
+    if (switcher.style.display === "none") {
+        switcher.style.display = "block";
+    } else {
+
+    }
+}
+
+switcher.addEventListener('change', () => {
+    if(this.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+    } else {
+        document.documentElement.setAttribute('data-theme', 'light');
+    }
+});
